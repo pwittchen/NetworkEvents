730a227b3883c4278be27c4e962211a9ca1a559c
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-08-07 19:42:58 +0200

    Removing StringHelper

--- app/src/main/java/pwittchen/com/internetconnectionstatelistener/helper/StringHelper.java
+++
@@ -1,9 +0,0 @@
-package pwittchen.com.internetconnectionstatelistener.helper;
-
-import android.content.Context;
-
-public class StringHelper {
-    public static String getString(Context context, int resourceId) {
-        return context.getResources().getString(resourceId);
-    }
-}
