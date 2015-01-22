5636b3e5b8b0327284de2ec872639e80b6197acd
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-01-22 09:54:22 +0100

    Completing documentation in code

--- network-events-library/src/main/java/com/github/pwittchen/networkevents/event/ConnectivityChanged.java
+++ network-events-library/src/main/java/com/github/pwittchen/networkevents/event/ConnectivityChanged.java
@@ -24,6 +24,7 @@ import com.github.pwittchen.networkevents.ConnectivityStatus;
  * Event pushed to Otto Event Bus when ConnectivityStatus changes;
  * E.g. when WiFi is turned on or off or when mobile network is turned on or off
  * it also occurs when WiFi is on, but there is no Internet connection or when there is Internet connection
+ * or when device goes off-line
  */
 public class ConnectivityChanged {
     private ConnectivityStatus connectivityStatus;
